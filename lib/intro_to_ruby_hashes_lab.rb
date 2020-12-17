9a5437de1d7367e4f782cca680c59773bcd9aa35
Adam Nowland <acnowland@gmail.com>
2020-12-17 20:03:24 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,31 @@
 def new_hash
   # return an empty hash
+  new_hash = {
+  }
+  return new_hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  second_hash = {
+    adam: 3
+  }
+  return second_hash
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {
+    name: 'Grace Hopper'
+  }
+  return new_hash
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id_hash = {
+    id: number
+  }
+  return id_hash
 end
